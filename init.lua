1647600d4a10c36acb6bcf137a84640e38638c65
Sheldon Lobo <sheldon.lobo@oracle.com>
2026-02-27 15:12:14 -0500

    relate .cc files to C++

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Sheldon: relate .cc to C++
+-- Add this to your init.lua
+vim.filetype.add({
+  extension = {
+    cc = "cpp",
+  },
+})
